c30eac702444d07a359c8ca7a6ce501537ecc31f
Livecummunicationj <167992371+Livecummunicationj@users.noreply.github.com>
2024-04-24 14:22:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_n3ag3dx", "template_7wuo638", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
